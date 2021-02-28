caa161539c1cebaa34e3d1bda173e3fb098b0202
Sebastian Dittrich <sebastian.dittrich@live.de>
2021-02-28 19:28:14 +0100

    Fixed exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,1 @@
-export { OpaqueModel, attribute } from "./Model";
-export { NoOpAdapter } from "./NoOpAdapter";
 export { OpaqueQueryBuilder } from "./OpaqueQueryBuilder";
-export { AdapterInterface } from "./contracts/AdapterInterface";
-export { AbstractOpaqueTable, OpaqueRow, OpaqueTable } from "./contracts/ModelContracts";
-export { QueryBuilderInterface } from "./contracts/QueryBuilderInterface";
